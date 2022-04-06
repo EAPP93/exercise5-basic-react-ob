9f59d18c2454d04dcf5be8a50c12df2418a49cfa
EAPP93 <empireeapp@gmail.com>
2022-04-07 01:06:58 +0200

    funcionalidad terminada agregar estilos

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Taskslist from './components/container/tasksList.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Taskslist></Taskslist>
+    </div>
+  );
+}
+
+export default App;
